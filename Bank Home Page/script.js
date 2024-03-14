68c4e906f367e84a28eb7a7102981acca64a13b9
Quach Truong Giang <truongjang.2306@gmail.com>
2024-03-14 20:57:22 +0700

    add function when we press link in nav, nav will disappear

--- Bank Home Page/script.js
+++ Bank Home Page/script.js
@@ -1,9 +1,23 @@
-const navbarToggleBtn = document.querySelector('.navbar-toggle-btn');
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarToggleBtn = document.querySelector(".navbar-toggle-btn");
+const navbarNav = document.querySelector(".navbar-nav");
+const navList = document.querySelectorAll(".navbar-nav a");
 
 const navbarToggleFunc = function () {
-    navbarToggleBtn.classList.toggle('active');
-    navbarNav.classList.toggle('active');
-}
+  navbarToggleBtn.classList.toggle("active");
+  navbarNav.classList.toggle("active");
+};
 
-navbarToggleBtn.addEventListener('click', navbarToggleFunc);
+navbarToggleBtn.addEventListener("click", navbarToggleFunc);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navList = document.querySelectorAll(".navbar-nav a");
+  var navbarNav = document.querySelector(".navbar-nav");
+  var navbarToggleBtn = document.querySelector(".navbar-toggle-btn");
+
+  navList.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navbarNav.classList.remove("active");
+        navbarToggleBtn.classList.remove("active");
+    });
+  });
+});
